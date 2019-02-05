c052c8d3a73513334a4e6488c3c61da2029c77c3
gooseandmegander <meganawilliamson@gmail.com>
2019-02-04 21:15:51 -0800

    snack class created

---
+++ src/snackBar/Snack.java
@@ -0,0 +1,53 @@
+package snackBar;
+
+public class Snack
+{
+	private static int maxID = 0;
+	private int id;
+	private String name;
+	private int quantity;
+	private double cost;
+	private int vendingID;
+
+	public Snack (String name, double cost, int quantity, int vendingID)
+	{	
+		maxID++;
+		id = maxID;
+		this.name = name;
+		this.cost = cost;
+		this.quantity = quantity;
+		this.vendingID = vendingID;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getQty()
+	{
+		return quantity;
+	}
+	
+	public void addQty(int qty)
+	{
+		this.quantity += qty;
+	}
+
+	public double getTotalCost(int qty)
+	{
+		return (double)qty * cost;
+	}
+
+	// buy snack given quantity
+	public double buySnack(int qty)
+	{
+		quantity -= qty;
+		return getTotalCost(qty);
+	}
+}
